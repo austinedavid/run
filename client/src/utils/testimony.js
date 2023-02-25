ea82c4a272d832bce6533318cc2be775fea76437
david <austinedavid96@gmail.com>
2023-02-25 15:49:08 +0100

    added some testimony pix

--- client/src/utils/testimony.js
+++ client/src/utils/testimony.js
@@ -1,13 +1,13 @@
-import face1 from '../images/face1.jfif';
-import face2 from '../images/face2.jpg';
-import face3 from '../images/face3.webp';
+import face1 from '../images/mama.jpg';
+import face2 from '../images/stanly.jpg';
+import face3 from '../images/mark.jpg';
 import face4 from '../images/face4.jpg';
 import face5 from '../images/face5.jpg';
 
 export const myTestimony = [
-    {sn: 1, name: "Maria Ochi", image: face1, writeUp: "I was very satisfied working with Gmodedev, he is very easy going and niceto work with. he always deliver his work at the expected time, without anyu delay, his workl is also pure and clean i really love how he work. yoo!! you guys can check and testify also"},
-    {sn: 2, name: "John Eze", image: face2, writeUp: "I was very satisfied working with Gmodedev, he is very easy going and niceto work with. he always deliver his work at the expected time, without anyu delay, his workl is also pure and clean i really love how he work. yoo!! you guys can check and testify also"},
-    {sn: 3, name: "Stanley Idoko", image: face3, writeUp: "I was very satisfied working with Gmodedev, he is very easy going and niceto work with. he always deliver his work at the expected time, without anyu delay, his workl is also pure and clean i really love how he work. yoo!! you guys can check and testify also"},
+    {sn: 1, name: "Maria Ochi", image: face1, writeUp: "I have had the opportunity of working with David on various occasions. One thing I must say is the fact that he gives feedback when necessary and is willing to take corrections where necessary. He is a very professional young man and has lots of potential to show the world."},
+    {sn: 2, name: "Stanley Idoko", image: face2, writeUp: "Austin is a Software developer with high class end to end customer satisfaction. He over delivered on our proposed project within a short period of time too. I recommend him for all"},
+    {sn: 3, name: "Mark Abba", image: face3, writeUp: "Over the years, working with David has been a pleasant experience. He is someone who will quickly get you ahead of others by completing his works at the appropriate time. I have always appreciated his well-written codes as a very skilled full-stack dev he is."},
     {sn: 4, name: "Mark Abba", image: face4, writeUp: "I was very satisfied working with Gmodedev, he is very easy going and niceto work with. he always deliver his work at the expected time, without anyu delay, his workl is also pure and clean i really love how he work. yoo!! you guys can check and testify also"},
     {sn: 5, name: "Kingsley Ekwueme", image: face5, writeUp: "I was very satisfied working with Gmodedev, he is very easy going and niceto work with. he always deliver his work at the expected time, without anyu delay, his workl is also pure and clean i really love how he work. yoo!! you guys can check and testify also"},
 ]
